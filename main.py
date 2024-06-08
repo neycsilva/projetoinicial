2ae2811cf4ecfdb80e04730a5627c7267b931df6
Sidney Cavalcante <neycsilva@hotmail.com>
2024-06-08 17:53:58 -0300

    meu primeiro commint

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Sidney Cavalcante")
+
+# Variáveis
+idade = 50
+nome = "Sidney"
+
+# Print
+print(idade)
+print(nome)
+
+# Soma
+nova_idade = idade
+nova_idade + 10
+
+# Listas
+lista = [1, 2, 3, 4, 5]
+lista
+print(type(lista))
+
+# Métodos
+lista_invertida = lista.reverse()
+print(lista_invertida)
